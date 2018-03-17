0a9a59e3e798992169cbbe672bb1047c889d2b28
Abhinav Abhi Verma <abhinavbb77@gmail.com>
2018-03-17 19:13:47 +0530

    apollo Client Changes

--- app/auth.js
+++ app/auth.js
@@ -1,6 +1,35 @@
 import { AsyncStorage } from "react-native";
+import { ApolloProvider } from 'react-apollo';
+ import { ApolloClient } from 'apollo-client';
+ import { HttpLink } from 'apollo-link-http';
+ import { InMemoryCache } from 'apollo-cache-inmemory';
+ import gql from 'graphql-tag';
 
 export const USER_KEY = "auth-demo-key";
+
+const CurrentUserForLayout = gql`
+query{
+authenticatedUser(email:"Purushotham.Raju@gmail.com",password:"Pregnant") {
+  userName
+  email
+  password
+}
+}
+`;
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
+  cache: new InMemoryCache()
+});
+
+client.query({ query: gql`query{
+  authenticatedUser(email:"Purushotham.Raju@gmail.com",password:"Pregnant") {
+    userName
+    email
+    password
+  }
+}` }).then(console.log);
+
+
 
 export const onSignIn = () => {
   console.log("handle sign in");
@@ -13,7 +42,7 @@ export const onSignOut = () => {
 };
 
 export const isSignedIn = () => {
-  debugger;
+  //debugger;
   return new Promise((resolve, reject) => {
     AsyncStorage.getItem(USER_KEY)
       .then(res => {
